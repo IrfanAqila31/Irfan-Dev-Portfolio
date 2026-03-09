ce39be518fbc483d7d8f1033f032c2fc7cd72151
IrfanAqila31 <irfanaqila31@gmail.com>
2026-03-09 14:20:07 +0800

    setup profil card

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,25 +1,20 @@
 import ProfileCard from "../components/ProfileCard";
-
+import avatar from "../assets/avatar.png";
 const AboutPage = () => {
   return (
     <>
       <div>
         <ProfileCard
-          className=""
           name="Irfan Aqila Utama"
-          title="Front End Developer"
-          handle="javicodes"
+          title="Web Developer"
+          handle="irfanAqila31"
           status="Online"
           contactText="Contact Me"
-          avatarUrl="./public/img/avatar.png"
-          showUserInfo={false}
+          avatarUrl={avatar}
+          showUserInfo={true}
           enableTilt={true}
           enableMobileTilt={false}
           onContactClick={() => console.log("Contact clicked")}
-          behindGlowColor="rgba(125, 190, 255, 0.67)"
-          iconUrl="./src/assets/demo/code.png"
-          behindGlowEnabled
-          innerGradient="linear-gradient(145deg,#60496e8c 0%,#71C4FF44 100%)"
         />
       </div>
     </>
